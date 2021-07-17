5b7ddd8669e9500875fdf6fe5b30bca2880e3e5c
Mohamed Kudratov <you@example.com>
2021-07-17 21:48:29 +0500

    'update'

---
+++ blog/views.py
@@ -0,0 +1,13 @@
+from django.views.generic import TemplateView, ListView, DetailView
+
+from blog.models import PostModel
+
+
+class BlogListView(ListView):
+    template_name = 'blog.html'
+    queryset = PostModel.objects.order_by('-pk')
+
+
+class BlogDetailView(DetailView):
+    template_name = 'blog-detail.html'
+    model = PostModel
--- online_shop/urls.py
+++ online_shop/urls.py
@@ -16,10 +16,14 @@ Including another URLconf
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('blog/', include('blog.urls', namespace='blog')),
+    path('products/', include('products.urls', namespace='products')),
+    path('orders/', include('orders.urls', namespace='orders')),
+    path('', include('pages.urls', namespace='pages ')),
 ]
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
